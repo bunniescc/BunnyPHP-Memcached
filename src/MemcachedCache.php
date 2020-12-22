c9ca1f0401d871311030fb5a3ee28771d6146c99
IvanLuLyf <835498692@qq.com>
2020-12-22 23:48:42 +0800

    update for BunnyPHP 2.6

--- src/MemcachedCache.php
+++ src/MemcachedCache.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace Bunny\Cache;
-
 
 use BunnyPHP\Cache;
 use Memcached;
@@ -21,23 +19,23 @@ class MemcachedCache implements Cache
         $this->memcached->addServers($config['servers']);
     }
 
-    public function get($key, $expire = 0)
+    public function get(string $key, $expire = 0)
     {
         return $this->memcached->get($key);
     }
 
-    public function has($key, $expire = 0)
+    public function has(string $key, $expire = 0): bool
     {
         $this->memcached->get($key);
         return $this->memcached->getResultCode() === Memcached::RES_SUCCESS;
     }
 
-    public function set($key, $value, $expire = 0)
+    public function set(string $key, $value, $expire = 0)
     {
         $this->memcached->set($key, $value, time() + $expire);
     }
 
-    public function del($key)
+    public function del(string $key)
     {
         $this->memcached->delete($key);
     }
